ee920f5b128756179a616421c8329d3785d3447b
jrs1498 <jsavage1498@gmail.com>
2014-05-05 14:11:13 -0400

    Fixed game loop

--- js/game.js
+++ js/game.js
@@ -1,110 +1,45 @@
+// city.js
 "use strict";
-
+// if app exists use the existing copy
+// else create a new object literal
 var app = app || {};
 
-app.game =
-{
-	// Constants
-	ZONESPERSIDE:		8,
-	ZONESIZE:		32,
-
-	// Variables - app
-	animID:			undefined,
-
-	// Variables - ThreeJs
-	renderer:		undefined,
-	scene:			undefined,
-	camera:			undefined,
-
-	/**
-	* init
-	*	Root initialization function for ZoneControl.
-	*/
-	init : function()
-	{
-		app.log("game.init()");
-		this.initThreeJs();
-		this.initGame();
-		app.log("--START GAME LOOP--");
-		this.gameLoop();
-	},
-
-	/**
-	* initThreeJs
-	*	Initializes everything required in order to use ThreeJs:
-	*	create the renderer, scene, camera, etc.
-	*/
-	initThreeJs : function()
-	{
-		app.log("game.initThreeJs()");
-	
-		this.renderer = new THREE.WebGLRenderer({antialias: true});
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
-		this.renderer.shadowMapEnabled = true;
-		document.body.appendChild(this.renderer.domElement);
-	
-		this.scene = new THREE.Scene();
+app.game = {
+    	// CONSTANT properties
+    	
+		// variable properties
+		renderer: undefined,
+		scene: undefined,
+		camera: undefined,
+		myobjects: [],
+		paused: false,
+		dt: 1/60,
+		controls: undefined,
 		
-		this.camera = new THREE.PerspectiveCamera(
-			75,
-			window.innerWidth / window.innerHeight,
-			1,
-			10000);
-		this.camera.position.x = 10;
-		this.camera.position.y = 10;
-		this.camera.position.z = 10;
-	},
-
-	/**
-	* initGame
-	*	Initializes everything necessary to play the game:
-	*	create the zones, players, etc.
-	*/
-	initGame : function()
+		
+    	init : function() 
 	{
-		app.log("game.initGame()");
-
-		// Test
-		var geo = new THREE.PlaneGeometry(2000, 2000, 40, 40);
-		var mat = new THREE.MeshPhongMaterial({color: 0xff00ff, overdraw: true});
-		var floor = new THREE.Mesh(geo, mat);
-		this.scene.add(floor);
-	},
-
-
-	// ---------------------------------------------------------------------------
-
-
-	/**
-	* gameLoop
-	*	Root gameLoop function. All game logic is contained in this function.
-	*/
-	gameLoop : function()
+		console.log('init called');
+		//this.setupThreeJS();
+		//this.setupWorld();
+		this.gameLoop();
+    	},
+    	
+	gameLoop: function()
 	{
+		app.animationID = requestAnimationFrame(this.gameLoop.bind(this));
 		this.update();
 		this.draw();
-
-		// NEEDS TO BE FIXED
-		// When using app.animationID it does not refresh for some reason
-		this.animID = requestAnimationFrame(this.gameLoop.bind(this));
-		//app.animationID = requestAnimationFrame(this.gameLoop.bind(this));
 	},
 
-	/**
-	* update
-	*	Primary game update logic, called once per frame.
-	*/
-	update : function()
+	update: function()
 	{
-		this.camera.position.y -= 0.5;
+		console.log("update");
 	},
 
-	/**
-	* draw
-	*	Primary game draw logic, called once per frame.
-	*/
-	draw : function()
+	draw: function()
 	{
-		this.renderer.render(this.scene, this.camera);
+		console.log("draw");
+		//this.renderer.render(this.scene, this.camera);
 	}
 };
